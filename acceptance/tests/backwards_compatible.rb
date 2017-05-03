dd21f44000347c28b9fe049ac8df49bb65933638
kevpl <ki@puppet.com>
2017-05-03 15:12:07 -0700

    (BKR-895) allow old require paths

---
+++ acceptance/tests/backwards_compatible.rb
@@ -0,0 +1,19 @@
+test_name 'backwards compatible test' do
+
+  step 'calls the new FOSSUtils even if I require & include the old path' do
+    assert( require 'beaker/dsl/install_utils/foss_utils' )
+    assert( !Beaker::DSL::InstallUtils::FOSSUtils::SourcePath.nil? )
+    assert( Beaker::DSL::InstallUtils::FOSSUtils.method_defined?(:lookup_in_env) )
+  end
+
+  step 'require old Helpers path, get helpers from new location' do
+    assert( require 'beaker/dsl/helpers/puppet_helpers' )
+    assert( Beaker::DSL::Helpers::PuppetHelpers.method_defined?(:puppet_user) )
+    assert( Beaker::DSL::Helpers::PuppetHelpers.method_defined?(:resolve_hostname_on) )
+  end
+
+  step 'require old Helpers module, get from new location' do
+    require 'beaker/dsl/helpers'
+    assert( Beaker::DSL::Helpers.is_a?( Module ) )
+  end
+end
