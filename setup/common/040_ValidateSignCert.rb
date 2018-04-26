41f12a0b93c1251101f589030d1e28d1c3c8ba43
Josh Cooper <josh@puppet.com>
2018-04-26 13:57:23 -0700

    (maint) Simplify logic for skipping agent that is also a master
    When the master host also has an agent role, we don't want to manually
    run `puppet agent -t`, because puppetserver will generate the
    self-signed CA and server certs when it starts. Rather than checking for
    the role, enumerate the agents, skipping the agent if it is also the
    master host.
    
    Add a comment about why we're deleting the contents of the ssldir
    instead of the directory.

--- setup/common/040_ValidateSignCert.rb
+++ setup/common/040_ValidateSignCert.rb
@@ -14,26 +14,27 @@ test_name "Validate Sign Cert" do
   step "Clear SSL on all hosts"
   hosts.each do |host|
     ssldir = on(host, puppet('agent --configprint ssldir')).stdout.chomp
+    # preserve permissions for master's ssldir so puppetserver can read it
     on(host, "rm -rf '#{ssldir}/*'")
   end
 
-  step "Master: Start Puppet Master" do
+  step "Start puppetserver" do
     master_opts = {
       :main => {
         :dns_alt_names => "puppet,#{hostname},#{fqdn}",
       },
     }
+    # server will generate the CA and server certs when it starts
     with_puppet_running_on(master, master_opts) do
-
-      hosts.each do |host|
-        next if host['roles'].include? 'master'
+      agents.each do |agent|
+        next if agent == master
 
         step "Agents: Run agent --test first time to gen CSR"
-        on host, puppet("agent --test --server #{master}"), :acceptable_exit_codes => [1]
+        on agent, puppet("agent --test --server #{master}"), :acceptable_exit_codes => [1]
       end
 
-      # Sign all waiting certs
-      step "Master: sign all certs"
+      # Sign all waiting agent certs
+      step "Server: sign all agent certs"
       on master, puppet("cert --sign --all"), :acceptable_exit_codes => [0,24]
 
       step "Agents: Run agent --test second time to obtain signed cert"
