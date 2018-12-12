20f1c27f3be324560ce552e071d9438607deb8f5
Casey Williams <caseywilliams@users.noreply.github.com>
2018-12-12 15:45:42 -0800

    (BKR-1560) Add stop_firewall_with_puppet_on helper
    - Extracts the StopFirewall setup task's logic into a new
      `stop_firewall_with_puppet_on` method for PuppetUtils.
    - Replaces the logic in the corresponding task with a call to this
      method.

--- lib/beaker-puppet/install_utils/puppet_utils.rb
+++ lib/beaker-puppet/install_utils/puppet_utils.rb
@@ -165,6 +165,29 @@ module Beaker
           end
         end
 
+        # Uses puppet to stop the firewall on the given hosts. Puppet must be installed before calling this method.
+        # @param [Host, Array<Host>, String, Symbol] hosts One or more hosts to act upon, or a role (String or Symbol) that identifies one or more hosts.
+        def stop_firewall_with_puppet_on(hosts)
+          block_on hosts do |host|
+            case host['platform']
+            when /debian/
+              result = on(host, 'which iptables', accept_all_exit_codes: true)
+              if result.exit_code == 0
+                on host, 'iptables -F'
+              else
+                logger.notify("Unable to locate `iptables` on #{host['platform']}; not attempting to clear firewall")
+              end
+            when /fedora|el-7/
+              on host, puppet('resource', 'service', 'firewalld', 'ensure=stopped')
+            when /el-|centos/
+              on host, puppet('resource', 'service', 'iptables', 'ensure=stopped')
+            when /ubuntu/
+              on host, puppet('resource', 'service', 'ufw', 'ensure=stopped')
+            else
+              logger.notify("Not sure how to clear firewall on #{host['platform']}")
+            end
+          end
+        end
       end
     end
   end
--- setup/common/025_StopFirewall.rb
+++ setup/common/025_StopFirewall.rb
@@ -1,22 +1,4 @@
 test_name "Stop firewall" do
   skip_test 'not testing with puppetserver' unless @options['is_puppetserver']
-  hosts.each do |host|
-    case host['platform']
-    when /debian/
-      result = on(host, 'which iptables', accept_all_exit_codes: true)
-      if result.exit_code == 0
-        on host, 'iptables -F'
-      else
-        logger.notify("Unable to locate `iptables` on #{host['platform']}; not attempting to clear firewall")
-      end
-    when /fedora|el-7/
-      on host, puppet('resource', 'service', 'firewalld', 'ensure=stopped')
-    when /el-|centos/
-      on host, puppet('resource', 'service', 'iptables', 'ensure=stopped')
-    when /ubuntu/
-      on host, puppet('resource', 'service', 'ufw', 'ensure=stopped')
-    else
-      logger.notify("Not sure how to clear firewall on #{host['platform']}")
-    end
-  end
+  stop_firewall_with_puppet_on(hosts)
 end
