2ca40f4ca56c7fa558869a38508c7f983d1b219a
Michael Hashizume <michael.hashizume@puppet.com>
2024-02-14 11:37:44 -0800

    Update release-prep script
    The release-prep script was originally taken from the RE repo standards
    repository: https://github.com/puppetlabs/release-engineering-repo-standards
    
    However, a more recent version of the script can be found in the Tefoji
    repo: https://github.com/puppetlabs/tefoji/blob/main/release-prep
    
    The version committed here has a lower Ruby version (as beaker-puppet is
    not yet compatible with Ruby >= 3.2).

--- release-prep.sh
+++ release-prep.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-docker run -it --rm -e CHANGELOG_GITHUB_TOKEN -v $(pwd):/usr/local/src/your-app \
+# Update Gemfile.lock
+docker run -t --rm \
+  -v $(pwd):/app \
+  ruby:3.1.4 \
+  /bin/bash -c 'apt-get update -qq && apt-get install -y --no-install-recommends git make netbase && cd /app && gem install bundler && bundle install --jobs 3; echo "LOCK_FILE_UPDATE_EXIT_CODE=$?"'
+
+docker run -t --rm -e CHANGELOG_GITHUB_TOKEN -v $(pwd):/usr/local/src/your-app \
   githubchangeloggenerator/github-changelog-generator:1.16.2 \
   github_changelog_generator --future-release $(grep VERSION lib/beaker-puppet/version.rb |rev |cut -d "'" -f 2 |rev)
