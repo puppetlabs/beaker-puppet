cf59f2d076af6cd52e884aceb9b5865be77c7fd9
Christopher Thorn <christopher.thorn@puppet.com>
2024-08-07 10:24:21 -0700

    (PA-6686) Add new Digicert to beaker-puppet solaris pre-suite

--- setup/common/003_solaris_cert_fix.rb
+++ setup/common/003_solaris_cert_fix.rb
@@ -40,6 +40,31 @@ DigiCert = <<~EOM
   -----END CERTIFICATE-----
 EOM
 
+DigiCertGlobalRootG2 = <<~EOM
+  -----BEGIN CERTIFICATE-----
+  MIIDjjCCAnagAwIBAgIQAzrx5qcRqaC7KGSxHQn65TANBgkqhkiG9w0BAQsFADBh
+  MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3
+  d3cuZGlnaWNlcnQuY29tMSAwHgYDVQQDExdEaWdpQ2VydCBHbG9iYWwgUm9vdCBH
+  MjAeFw0xMzA4MDExMjAwMDBaFw0zODAxMTUxMjAwMDBaMGExCzAJBgNVBAYTAlVT
+  MRUwEwYDVQQKEwxEaWdpQ2VydCBJbmMxGTAXBgNVBAsTEHd3dy5kaWdpY2VydC5j
+  b20xIDAeBgNVBAMTF0RpZ2lDZXJ0IEdsb2JhbCBSb290IEcyMIIBIjANBgkqhkiG
+  9w0BAQEFAAOCAQ8AMIIBCgKCAQEAuzfNNNx7a8myaJCtSnX/RrohCgiN9RlUyfuI
+  2/Ou8jqJkTx65qsGGmvPrC3oXgkkRLpimn7Wo6h+4FR1IAWsULecYxpsMNzaHxmx
+  1x7e/dfgy5SDN67sH0NO3Xss0r0upS/kqbitOtSZpLYl6ZtrAGCSYP9PIUkY92eQ
+  q2EGnI/yuum06ZIya7XzV+hdG82MHauVBJVJ8zUtluNJbd134/tJS7SsVQepj5Wz
+  tCO7TG1F8PapspUwtP1MVYwnSlcUfIKdzXOS0xZKBgyMUNGPHgm+F6HmIcr9g+UQ
+  vIOlCsRnKPZzFBQ9RnbDhxSJITRNrw9FDKZJobq7nMWxM4MphQIDAQABo0IwQDAP
+  BgNVHRMBAf8EBTADAQH/MA4GA1UdDwEB/wQEAwIBhjAdBgNVHQ4EFgQUTiJUIBiV
+  5uNu5g/6+rkS7QYXjzkwDQYJKoZIhvcNAQELBQADggEBAGBnKJRvDkhj6zHd6mcY
+  1Yl9PMWLSn/pvtsrF9+wX3N3KjITOYFnQoQj8kVnNeyIv/iPsGEMNKSuIEyExtv4
+  NeF22d+mQrvHRAiGfzZ0JFrabA0UWTW98kndth/Jsw1HKj2ZL7tcu7XUIOGZX1NG
+  Fdtom/DzMNU+MeKNhJ7jitralj41E6Vf8PlwUHBHQRFXGU7Aj64GxJUTFy8bJZ91
+  8rGOmaFvE7FBcf6IKshPECBV1/MUReXgRPTqh5Uykw7+U0b6LJ3/iyK5S9kJRaTe
+  pLiaWN0bfVKfjllDiIGknibVb63dDcY3fe0Dkhvld1927jyNxF1WW6LZZm6zNTfl
+  MrY=
+  -----END CERTIFICATE-----
+EOM
+
 USERTrust = <<~EOM
   -----BEGIN CERTIFICATE-----
   MIIF3jCCA8agAwIBAgIQAf1tMPyjylGoG7xkDjUDLTANBgkqhkiG9w0BAQwFADCBiDELMAkGA1UE
@@ -79,6 +104,11 @@ hosts.each do |host|
   on(host, 'chmod a+r /root/DigiCert_Trusted_Root_G4.pem')
   on(host, 'cp -p /root/DigiCert_Trusted_Root_G4.pem /etc/certs/CA/')
   on(host, 'rm /root/DigiCert_Trusted_Root_G4.pem')
+
+  create_remote_file(host, 'DigiCert_Trusted_Root2_G4.pem', DigiCertGlobalRootG2)
+  on(host, 'chmod a+r /root/DigiCert_Trusted_Root2_G4.pem')
+  on(host, 'cp -p /root/DigiCert_Trusted_Root2_G4.pem /etc/certs/CA/')
+  on(host, 'rm /root/DigiCert_Trusted_Root2_G4.pem')
 
   if host.platform =~ /solaris-11-sparc/
     create_remote_file(host, 'USERTrust_RSA_Certification_Authority.pem', USERTrust)
